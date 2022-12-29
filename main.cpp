308b604a1f8e5d684c141b3de5ece4c710afe718
slyinstinct09 <wilmerenriq09@outlook.com>
2022-12-29 11:20:52 -0500

    updating main.cpp
    Adding code to display blank window

--- main.cpp
+++ main.cpp
@@ -9,7 +9,29 @@
 #include <iostream>
 
 int main() {
-    
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // Creating window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Infinity Space", nullptr, nullptr);
+
+    // Obtaining number of extensions supported by GPU
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vector;
+    auto test = matrix * vector;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 
     return 0;
 }
